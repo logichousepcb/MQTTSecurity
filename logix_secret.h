b9942418bcfdd4894029862cb9fc94bd12392418
logichousepcb <73470843+logichousepcb@users.noreply.github.com>
2021-02-17 06:52:23 -0500

    Create logix_secret.h

---
+++ logix_secret.h
@@ -0,0 +1,7 @@
+#define USER_SSID                 ""                              //WiFi username
+#define USER_PASSWORD             ""                              //WiFi password
+#define USER_MQTT_SERVER          "192.168.1.130"                 //enter MQTT server ip
+#define USER_MQTT_PORT            1883
+#define USER_MQTT_USERNAME        ""                              //MQTT username
+#define USER_MQTT_PASSWORD        ""                              //MQTT password
+#define USER_MQTT_CLIENT_NAME     "Rack64"         // Used to define MQTT topics, MQTT Client ID, and ArduinoOTA
